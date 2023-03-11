4d959f559d8575738ecbdd73bc9730c46a31d6a9
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-11 05:42:32 +0330

    Function: isValidCommand

--- regular-chatbot.js
+++ regular-chatbot.js
@@ -1,1 +1,12 @@
-// look back again and read more ... 
+// look back again and read more ...
+
+function isValidCommand(command) {
+  const REGEX = /^chatbot/i;
+  return REGEX.test(command);
+}
+
+// console.log(isValidCommand("Chatbot, play a song from the 80's."));
+// console.log(isValidCommand("Hey Chatbot, where is the closest pharmacy?"));
+// console.log(
+//   isValidCommand("CHATBOT, do you have a solution for this challenge?")
+// );
